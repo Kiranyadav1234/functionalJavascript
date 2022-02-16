085787daaa986cc31647a63c49fa8f2fc2861d1f
Kiranyadav1234 <Kiran_Yadav@mckinsey.com>
2022-02-16 09:45:59 +0530

    feat:call function given number of times

---
+++ highOrderFunctionGenerator.js
@@ -0,0 +1,14 @@
+
+
+
+    function repeat(operation, num) {
+        if (typeof num !== 'number'){
+            throw new Error('Not a valid input type for num.');
+        }
+        if(num > 0){
+            operation();
+            repeat(operation,num-1);
+        }
+      }
+    
+      module.exports = repeat;
