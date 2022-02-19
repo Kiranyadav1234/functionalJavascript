35681e5f7d4962caa966f4c3431155282f0835e6
Kiranyadav1234 <Kiran_Yadav@mckinsey.com>
2022-02-19 10:35:20 +0530

    feat:add extract shorter message(length<50)

--- filter.js
+++ filter.js
@@ -2,15 +2,9 @@ function getShortMessages(messages) {
     if (! Array.isArray(messages)){
         throw new Error('Not a valid input type for messages.')
     };
-    const messageBox=[];
-   const quotes=messages.filter((data)=>{
-      
-       if(data.message.length<50)
-       {
-           messageBox.push(data.message);
-       }
-   });
-   return messageBox;
+    
+  return  messages.filter(data=>data.message.length<50);
+  
   }
 
   module.exports = getShortMessages
