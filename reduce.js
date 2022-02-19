ba3fe8630254cf3c5d94cc61a1be8179718de2bc
Kiranyadav1234 <Kiran_Yadav@mckinsey.com>
2022-02-19 10:36:52 +0530

    feat:add count of occurance of word

--- reduce.js
+++ reduce.js
@@ -1,10 +1,16 @@
-function countWords(inputWords) {
+const countWords=(inputWords)=>{
     if( !Array.isArray(inputWords))
     {
         throw new Error('invalid input');
     }
     return inputWords.reduce((countOfWord,word)=>{
-        countOfWord[word]=++countOfWord[word]||1;
+        if(word in countOfWord)
+        {
+            countOfWord[word]++;
+        }
+        else{
+            countOfWord[word]=1;
+        }
         return countOfWord;
     },{});
   
